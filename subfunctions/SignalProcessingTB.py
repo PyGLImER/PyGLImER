LQT rotation implemented, but not fully tested (specifically, the correct labeling might be unstable..

subfunctions/SignalProcessingTB.py
@@ -146,14 +146,14 @@ def sshift(s,N2,dt,shift):
 #         name.append(item[-1]) #name of the boundary
     
 def rotate_LQT(st):
-    #13.02 For some reason this alters also the input stream?
     """rotates a stream given in RTZ to LQT using Singular Value Decomposition
     INPUT:
         st: stream given in RTZ
     RETURNS:
         LQT: stream in LQT"""
     dt = st[0].stats.delta # sampling interval
-    LQT = st
+    LQT = st.copy()
+    del st
     
     # 1. Find which component is which one and put them in a dict
     stream = {
@@ -171,7 +171,7 @@ def rotate_LQT(st):
     # the maximum energy around theoretical S-wave arrival - that one would
     # be Q
     tas = config.tz/dt #theoretical arrival sample
-    # enery windows - 3 seconds before and after (is that enough?)
+    # enery windows - 5 seconds before and after (is that enough?)
     ws = round(tas - 5/dt)
     we = round(tas + 5/dt)
     a = np.sum(np.square(vh[0,ws:we]))
@@ -179,6 +179,9 @@ def rotate_LQT(st):
     
     # maybe I should have a factor of 1.5 here? See how it works out
     # 13.02: ok, that doesn't work should think of a different method
+    # It seems like the iasp91 arrival is not precise enough
+    # Maybe it is also the data. The testdata was downloaded for different
+    # epicentral distance, so perhaps I'm seeing different phases
     if a > b:
         Q = vh[0]
         L = vh[1]
@@ -186,7 +189,7 @@ def rotate_LQT(st):
         Q = vh[1]
         L = vh[0]
     
-    # 3. save L and Q trace and change the label of the stream
+    #3. save L and Q trace and change the label of the stream
     for tr in LQT:
         if tr.stats.channel[2] == "R":
             tr.stats.channel = tr.stats.channel[0:2] +"Q"

subfunctions/preprocess.py
@@ -31,6 +31,7 @@ from obspy.signal import filter
 #from importlib import import_module
 import shelve
 import numpy as np
+from subfunctions.SignalProcessingTB import rotate_LQT
 
 
 
@@ -291,6 +292,13 @@ def preprocess(taper_perc,taper_type,event_cat,webclient,model):
                             
                         st.rotate(method='NE->RT',inventory=station_inv,back_azimuth=result["backazimuth"])
                         
+                        # Rotate to LQT
+                        if config.rot == "LQT":
+                            st = rotate_LQT(st)
+                        elif config.rot == "PSS":
+                            continue #to be implemented
+                        
+                        
                         
                     ##### OVERWRITE OLD STRING FILE #####
                         # That's a bad idea because in the next iteration it will not be able anymore to find response files for the rotated channels
@@ -305,15 +313,11 @@ def preprocess(taper_perc,taper_type,event_cat,webclient,model):
                         # according to the original Matlab script, we will be workng with several bandpass (low-cut) filters to evaluate the SNR
                         # Then, the SNR will be evaluated for each of the traces and the traces will be accepted or rejected depending on their SNR.
                         
-                        #Find channel type - obspy uses the order T-R-Z - that not true I will have to change this
                         #create stream dictionary
-                        """13.02.2020 BUG: THe order is not always T-R-Z but I think the old
-                        bug occurred due to the Z-trace sometimes being called 3"""
-                        # This way is actually not robust
-                        # stream = {
-                        #         st[0].stats.channel[2]: st[0].data,
-                        #         st[1].stats.channel[2]: st[1].data,
-                        #         st[2].stats.channel[2]: st[2].data}
+                        stream = {
+                                st[0].stats.channel[2]: st[0].data,
+                                st[1].stats.channel[2]: st[1].data,
+                                st[2].stats.channel[2]: st[2].data}
                         # 25.08.2019
                         # 
                         
@@ -341,10 +345,12 @@ def preprocess(taper_perc,taper_type,event_cat,webclient,model):
                             noisemat = np.zeros((len(config.lowco),3),dtype=float) #matrix to save SNR
                             
                             for ii,f in enumerate(config.lowco):
-                                ftcomp = filter.bandpass(st[0], f,4.99, sampling_f, corners=4, zerophase=True)
-                                frcomp = filter.bandpass(st[1], f,4.99, sampling_f, corners=4, zerophase=True)
-                                fzcomp = filter.bandpass(st[2], f,4.99, sampling_f, corners=4, zerophase=True)      
-                                
+                                ftcomp = filter.bandpass(stream["T"], f,4.99, sampling_f, corners=4, zerophase=True)
+                                frcomp = filter.bandpass(stream["R"], f,4.99, sampling_f, corners=4, zerophase=True)
+                                if "Z" in stream:
+                                    fzcomp = filter.bandpass(stream["Z"], f,4.99, sampling_f, corners=4, zerophase=True)      
+                                elif "3" in stream:
+                                    fzcomp = filter.bandpass(stream["3"], f,4.99, sampling_f, corners=4, zerophase=True)
                                 # Compute the SNR for given frequency bands
                                 snrr = (sum(np.square(frcomp[pts1:pts2]))/npts)/(sum(np.square(frcomp[ptn1:ptn2]))/nptn)
                                 snrr2 = (sum(np.square(frcomp[ptp1:ptp2]))/nptp)/(sum(np.square(frcomp[ptn1:ptn2]))/nptn)
@@ -365,10 +371,18 @@ def preprocess(taper_perc,taper_type,event_cat,webclient,model):
                                 if snrr > config.SNR_criteria[0] and snrr2/snrr < config.SNR_criteria[1] and snrz > config.SNR_criteria[2]: #accept
                                     crit = True
                                     # overwrite the old traces with the sucessfully filtered ones
-                                    st[0].data = ftcomp
-                                    st[1].data = frcomp
-                                    st[2].data = fzcomp
-                                    break #waveform is accepted no further tests needed
+                                    # st[0].data = ftcomp
+                                    # st[1].data = frcomp
+                                    # st[2].data = fzcomp
+                                for tr in st:
+                                    if tr.stats.channel[2] == "R":
+                                        tr.data = frcomp
+                                    elif tr.stats.channel[2] == "Z" or tr.stats.channel[2] == "3":
+                                        tr.data = fzcomp
+                                    elif tr.stats.channel[2] == "T":
+                                        tr.data = ftcomp
+                                        
+                                break #waveform is accepted no further tests needed
                             
                             if not crit:
                                 raise SNRError(noisemat)