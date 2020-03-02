Added rating function, fixed PSvSh rotation

subfunctions/SignalProcessingTB.py
@@ -116,7 +116,7 @@ def rotate_PSV(statlat, statlon, rayp, st):
     INPUT:
         statlat: station latitude
         statlon: station longitude
-        rayp: rayparameter in s/m
+        rayp: rayparameter in s/m (i.e. horizontal slowness)
         st: stream in RTZ"""
     x = subprocess.Popen([config.lith1, "-p", str(statlat),
                           str(statlon)], stdout=subprocess.PIPE)
@@ -138,7 +138,7 @@ def rotate_PSV(statlat, statlon, rayp, st):
         name.append(item[-1])  # name of the boundary
 
     # build weighted average for upper 15km
-    maxd = 10e3
+    maxd = 15e3
     for ii, item in enumerate(depth):
         if item <= maxd:
             break
@@ -148,33 +148,37 @@ def rotate_PSV(statlat, statlon, rayp, st):
     avs = np.multiply(vs[ii+1:], -np.diff(depth)[ii:])
     avs = sum(avs) + vs[ii]*(-np.diff(depth)[ii-1] + maxd - depth[ii-1])
     avs = avs/maxd
-    # 10.02: Ok, I checked the values for the rayp, avs, and avp. They
-    # seem alright. However, P and Sv do look almost identical.
 
-    # # Do the rotation as in Rondenay (2009)
-    # qa = np.sqrt(1/avp**2 - rayp**2)
-    # qb = np.sqrt(1/avs**2 - rayp**2)
-    # a = avs**2*rayp**2 - .5
-    # rotmat = np.array([[a/(avp*qa), rayp*avs**2/avp, 0],
-    #                    [rayp*avs, -a/(avs*qb), 0],
-    #                    [0, 0, 0.5]])
+    # Do the rotation as in Rondenay (2009)
+    # Note that in contrast to the paper the z-components have to be
+    # negated as it uses a definition, where depth is positive
+    qa = np.sqrt(1/avp**2 - rayp**2)
+    qb = np.sqrt(1/avs**2 - rayp**2)
+    a = avs**2*rayp**2 - .5
+    rotmat = np.array([[-a/(avp*qa), rayp*avs**2/avp, 0],
+                        [-rayp*avs, -a/(avs*qb), 0],
+                        [0, 0, 0.5]])
 
 
     # Bostock (1999)
-    qa = np.sqrt(1/avp**2 - rayp**2)
-    qb = np.sqrt(1/avs**2 - rayp**2)
-    Delta = 1 - 4*rayp**2*avs**2 + 4*rayp**4*avs**4 + 4*avs**4*rayp**2*qa*qb
-    rotmat = np.array([[-2*avp*qa*(1 - 2*rayp**2*avs**2)/Delta,
-                        4*avp*avs**2*rayp*qa*qb/Delta, 0],
-                       [4*avs**3*rayp*qa*qb/Delta,
-                        2*avs*qb*(1-2*rayp**2*avs**2)/Delta, 0],
-                       [0, 0, .5]])
+    # That doesn't work
+    # qa = np.sqrt(1/avp**2 - rayp**2)
+    # qb = np.sqrt(1/avs**2 - rayp**2)
+    # Delta = 1 - 4*rayp**2*avs**2 + 4*rayp**4*avs**4 + 4*avs**4*rayp**2*qa*qb
+    # rotmat = np.array([[-2*avp*qa*(1 - 2*rayp**2*avs**2)/Delta,
+    #                     4*avp*avs**2*rayp*qa*qb/Delta, 0],
+    #                    [4*avs**3*rayp*qa*qb/Delta,
+    #                     2*avs*qb*(1-2*rayp**2*avs**2)/Delta, 0],
+    #                    [0, 0, .5]])
     # 1. Find which component is which one and put them in a dict
     stream = {
         st[0].stats.channel[2]: st[0].data,
         st[1].stats.channel[2]: st[1].data,
         st[2].stats.channel[2]: st[2].data}
     PSvSh = st.copy()
+    # Filter
+    if config.phase == "S":
+        PSvSh.filter('bandpass', freqmin=.03, freqmax=.33, zerophase=True)
     del st
     # create input matrix, Z component is sometimes called 3
     if "Z" in stream:
@@ -246,7 +250,7 @@ def rotate_LQT(st):
     # this:
     if config.phase == "S":
         ews = 1
-        ewe = 10
+        ewe = 5
     elif config.phase == "P":
         ews = 1
         ewe = 8

subfunctions/archive/rot_test.py
@@ -8,7 +8,10 @@ Created on Thu Feb 20 13:59:37 2020
 
 import numpy as np
 from obspy.core import *
-
+from matplotlib import pyplot as plt
+from matplotlib.ticker import ScalarFormatter, AutoMinorLocator
+import matplotlib as mpl
+wdir = '/home/pm/Documents/Masters/GLimer to Obspy/'
 
 # INPUT PARAMETERS
 NEZ_file = "rot.tr"
@@ -25,7 +28,7 @@ rayp = 3e-5
 def main(NEZ_file, RTZ_file, PSS_file, N):
     NEZ, RTZ, PSS = read_ray_in(NEZ_file, RTZ_file, PSS_file, N)
     PSS_mine, rotmat = rot(avp, avs, rayp, RTZ)
-    return PSS_mine, PSS, RTZ, rotmat
+    return PSS_mine, PSS, RTZ, rotmat, NEZ
 
 def read_ray_in(NEZ_file, RTZ_file, PSS_file, N):
     """Read synthetic waveforms
@@ -55,28 +58,59 @@ def read_ray_in(NEZ_file, RTZ_file, PSS_file, N):
     return NEZ, RTZ, PSS
 
 def rot(avp, avs, rayp, RTZ):
-    qa = np.sqrt(1/avp**2 - rayp**2)
-    qb = np.sqrt(1/avs**2 - rayp**2)
-    Delta = 1 - 4*rayp**2*avs**2 + 4*rayp**4*avs**4 + 4*avs**4*rayp**2*qa*qb
-    rotmat = np.array([[-2*avp*qa*(1 - 2*rayp**2*avs**2)/Delta,
-                        4*avp*avs**2*rayp*qa*qb/Delta, 0],
-                        [4*avs**3*rayp*qa*qb/Delta,
-                        2*avs*qb*(1-2*rayp**2*avs**2)/Delta, 0],
-                        [0, 0, .5]])
-    
-    # Do the rotation as in Rondenay (2009)
+    # somehow the '99 rotation still doesn't work
     # qa = np.sqrt(1/avp**2 - rayp**2)
     # qb = np.sqrt(1/avs**2 - rayp**2)
-    # a = avs**2*rayp**2 - .5
-    # rotmat = np.array([[a/(avp*qa), rayp*avs**2/avp, 0],
-    #                     [rayp*avs, -a/(avs*qb), 0],
-    #                     [0, 0, 0.5]])
+    # Delta = 1 - 4*rayp**2*avs**2 + 4*rayp**4*avs**4 + 4*avs**4*rayp**2*qa*qb
+    # rotmat = np.array([[-2*avp*qa*(1 - 2*rayp**2*avs**2)/-Delta,
+    #                     4*avp*avs**2*rayp*qa*qb/Delta, 0],
+    #                     [4*avs**3*rayp*qa*qb/-Delta,
+    #                     2*avs*qb*(1-2*rayp**2*avs**2)/Delta, 0],
+    #                     [0, 0, .5]])
+    
+    # Do the rotation as in Rondenay (2009)
+    qa = np.sqrt(1/avp**2 - rayp**2)
+    qb = np.sqrt(1/avs**2 - rayp**2)
+    a = avs**2*rayp**2 - .5
+    rotmat = np.array([[-a/(avp*qa), rayp*avs**2/avp, 0],
+                        [-rayp*avs, -a/(avs*qb), 0],
+                        [0, 0, 0.5]])
     A_in = np.array([RTZ[2],
                 RTZ[0],
                 RTZ[1]])
     PSS = np.dot(rotmat, A_in)
     return PSS,rotmat
 
-PSS_mine, PSS, RTZ, rotmat = main(NEZ_file, RTZ_file, PSS_file, N)
+PSS_mine, PSS, RTZ, rotmat, NEZ = main(NEZ_file, RTZ_file, PSS_file, N)
+
+
+plt.style.use('data/PaperDoubleFig.mplstyle')
+# Make some style choices for plotting
+colourWheel = ['#329932', '#ff6961', 'b', '#6a3d9a', '#fb9a99', '#e31a1c',
+                '#fdbf6f', '#ff7f00', '#cab2d6', '#6a3d9a', '#ffff99',
+                '#b15928', '#67001f', '#b2182b', '#d6604d', '#f4a582',
+                '#fddbc7', '#f7f7f7', '#d1e5f0', '#92c5de', '#4393c3',
+                '#2166ac', '#053061']
+dashesStyles = [[3, 1], [1000, 1], [2, 1, 10, 1], [4, 1, 1, 1, 1, 1]]
+
+y = NEZ[2]
+# create time vector
+plt.close('all')
+fig, ax = plt.subplots(1, 1, sharex=True)
+ax.set_xlabel('time in s')
+
+ax.plot(y, color="k",linewidth=2)
+#x.set_xlim(-20, 50)
+#x.set_title(ch[ii])
+#ax.yaxis.set_major_formatter(ScalarFormatter())
+#ax.yaxis.major.formatter._useMathText = True
+ax.xaxis.set_major_formatter(ScalarFormatter())
+ax.xaxis.major.formatter._useMathText = True
+#x.yaxis.set_minor_locator(AutoMinorLocator(5))
+ax.xaxis.set_minor_locator(AutoMinorLocator(5))
+# x.yaxis.set_label_coords(0.63, 1.01)
+#x.yaxis.tick_right()
+ax.yaxis.remove()
+    #ax.grid(color='c', which='both', linewidth=.25)
 
         

subfunctions/createRF.py
@@ -64,7 +64,20 @@ def createRF(st, dt):
     return RF
 
 
-def MoveoutStack(station, network, phasen=config.phase, TAT=config.tz):
+def stackRF(network, station, phase=config.phase):
+    # moveout correction
+    RF_mo = Moveout(station, network, phasen=phase)
+    # create trace for stack
+    stack = RF_mo[0]
+    stack[0].data = np.zeros(stack[0].stats.npts)
+    for st in RF_mo:
+        st.normalize()  # again, just to be safe
+        stack[0].data = stack[0].data + st[0].data
+    stack.normalize()
+    return stack
+
+
+def Moveout(station, network, phasen=config.phase):
     """Moveout corretion for pS and sP phases. A modified version of the
     original Fortran program written by Xiaohui Yuan
     INPUT:
@@ -82,21 +95,31 @@ def MoveoutStack(station, network, phasen=config.phase, TAT=config.tz):
     for file in os.listdir(ioloc):
         if file[:4] == "info":
             continue
-        st = read(file)
+        try:
+            st = read(ioloc + '/' + file)
+        except IsADirectoryError:
+            continue
         st.normalize()  # make sure traces are normalised
-        RF.append(st[0].data)
-    RF = np.array(RF)
-    dt = st[0].stats.delta
+        RF.append(st.copy())
+    # dt = st[0].stats.delta
     # read info file
     with shelve.open(ioloc + '/info') as info:
         rayp = info["rayp"]
         onset = info["onset"]
     rayp = np.array(rayp)  # create numpy array
-    rayp_km = rayp*1000  # in s/km
+    # rayp_km = rayp*1000  # in s/km
     rayp = 111319.9*rayp  # from s/m to s/deg
-    
+    model = load_model()
+    for ii, st in enumerate(RF):
+        st = model.moveout(st, onset[ii], rayp[ii], phase=phase)
+    return RF
 
-def load_model(fname='iasp91'):
+
+""" Everything from here on is from the RF model by Tom Eulenfeld, modified
+by me"""
+
+
+def load_model(fname='iasp91.dat'):
     """
     Load model from file.
 
@@ -255,14 +278,14 @@ class SimpleModel(object):
             index0 = int(floor((onset - st.starttime) * st.sampling_rate))
             t0, t1 = self.stretch_delay_times(rayp, phase=phase,
                                               ref=ref)
-            # S_multiple = phase[0].upper() == 'S' and len(phase) > 3
-            # if S_multiple:
-            #     time0 = st.starttime - st.onset + index0 * st.delta
-            #     old_data = tr.data[:index0][::-1]
-            #     t = -time0 - np.arange(index0) * st.delta
-            #     new_t = -np.interp(-t, -t0, -t1, left=0, right=0)
-            #     data = np.interp(-t, -new_t, old_data, left=0., right=0.)
-            #     tr.data[:index0] = data[::-1]
+            S_multiple = phase[0].upper() == 'S' and len(phase) > 3
+            if S_multiple:
+                time0 = st.starttime - st.onset + index0 * st.delta
+                old_data = tr.data[:index0][::-1]
+                t = -time0 - np.arange(index0) * st.delta
+                new_t = -np.interp(-t, -t0, -t1, left=0, right=0)
+                data = np.interp(-t, -new_t, old_data, left=0., right=0.)
+                tr.data[:index0] = data[::-1]
             if t0[-1] > t1[-1]:
                 index0 += 1
             time0 = st.starttime - onset + index0 * st.delta
@@ -271,8 +294,12 @@ class SimpleModel(object):
             # stretch old times to new times
             new_t = np.interp(t, t0, t1, left=0, right=None)
             # interpolate data at new times to data samples
-            data = np.interp(t, new_t, old_data, left=None, right=0.)
-            tr.data[index0:] = data
+            print(t)
+            try:
+                data = np.interp(t, new_t, old_data, left=None, right=0.)
+                tr.data[index0:] = data
+            except ValueError:
+                continue
         return stream
 
 

subfunctions/plot.py
@@ -3,7 +3,8 @@
 """
 Created on Sat Feb 22 11:50:58 2020
 
-Module to plot waveforms from receiver functions
+Module to plot waveforms from receiver functions.
+Has to be started with working directory as upper directory of GLImER
 @author: pm
 """
 import os
@@ -21,7 +22,7 @@ import subprocess
 # plotting properties
 # dirFile = os.path.dirname('main.py')
 
-plt.style.use('PaperDoubleFig.mplstyle')
+plt.style.use('data/PaperDoubleFig.mplstyle')
 # Make some style choices for plotting
 colourWheel = ['#329932', '#ff6961', 'b', '#6a3d9a', '#fb9a99', '#e31a1c',
                '#fdbf6f', '#ff7f00', '#cab2d6', '#6a3d9a', '#ffff99',
@@ -50,7 +51,11 @@ def plot_all_RF(phase, network, station, TAT=config.tz, dpi=300):
         if file[:4] == "info":   # Skip the info files, they are none for RF
             # but doesn't hurt
             continue
-        RF = read(inloc + file)
+        try:
+            RF = read(inloc + file)
+        except IsADirectoryError as e:
+            print(e)
+            continue
         dt = RF[0].stats.delta
         N = RF[0].stats.npts
         y = RF[0].data
@@ -61,30 +66,46 @@ def plot_all_RF(phase, network, station, TAT=config.tz, dpi=300):
         else:
             TATp = TAT
         # shorten vector
-        y = y[:round((TATp+50)/dt)]
+            
+        # y = y[:round((TATp+50)/dt)]
         # create time vector
-        t = np.linspace(0-TATp, 50, len(y))
+        t = np.linspace(0-TATp, config.ta, len(y))
         plt.close('all')
         fig = plt.figure()
         fig, ax = plt.subplots(1, 1, sharex=True)
-        ax.axvline(color='r')  # should plot at x = 0
-        ax.plot(t, y, color="k")
-        ax.set_ylabel('normalised Amplitude')
-        ax.set_xlabel('time in s')
+        # Move left y-axis and bottim x-axis to centre, passing through (0,0)
+        ax.spines['bottom'].set_position("center")
+        ax.spines['left'].set_visible(False)
+        
+        # Eliminate upper and right axes
+        ax.spines['right'].set_color('none')
+        ax.spines['top'].set_color('none')
+        
+        # Show ticks in the left and lower axes only
+        ax.xaxis.set_ticks_position('bottom')
+        # ax.axvline(color='r')  # should plot at x = 0
+        ax.plot(t, y, color="k", linewidth=1.5)
+        # ax.set_ylabel('normalised Amplitude')
+        ax.set_ylabel('time in s')
+        # ax.set_position([.5, .5, .1, .1])
         x = ax
-        x.set_xlim(-20, 50)
-        x.yaxis.set_major_formatter(ScalarFormatter())
-        x.yaxis.major.formatter._useMathText = True
+        x.set_xlim(0, 30)
+        x.set_ylim(-0.6, .6)
         x.xaxis.set_major_formatter(ScalarFormatter())
+        x.yaxis.major.formatter._useMathText = True
+        x.yaxis.set_major_formatter(plt.NullFormatter())
         x.xaxis.major.formatter._useMathText = True
         x.yaxis.set_minor_locator(AutoMinorLocator(5))
         x.xaxis.set_minor_locator(AutoMinorLocator(5))
-        plt.grid(color='c', which="both", linewidth=.25)
+        x.tick_params(axis ='y', which ='both', length = 0)
+        # plt.grid(color='c', which="both", linewidth=.25)
         # x.yaxis.set_label_coords(0.63, 1.01)
-        x.yaxis.tick_right()
+        # x.yaxis.tick_right()
         # ax.legend(frameon=False, loc='upper left', ncol=2, handlelength=4)
         # plt.legend(['it_max=4000', 'it_max=400'])
         plt.savefig(os.path.join(outloc + file[:-5] + "pdf"), dpi=dpi)
+        # ax.xaxis.set_ticks_position('left')
+
 
 
 def plot_all_wav(phase, network, station, TAT=config.tz, dpi=300):
@@ -106,7 +127,11 @@ def plot_all_wav(phase, network, station, TAT=config.tz, dpi=300):
     for file in os.listdir(inloc):
         if file[:4] == "info":  # Skip the info files
             continue
-        st = read(inloc + file)
+        try:
+            st = read(inloc + file)
+        except IsADirectoryError as e:
+            print(e)
+            continue
         dt = st[0].stats.delta
         N = st[0].stats.npts
         y = []
@@ -114,35 +139,38 @@ def plot_all_wav(phase, network, station, TAT=config.tz, dpi=300):
         for tr in st:
             y.append(tr.data)
             ch.append(tr.stats.channel[2])
-        if phase == "S":  # flip trace
-            y = np.flip(y)
-            # y = -y  # polarity
-            TATp = (N-1)*dt - TAT
-        else:
-            TATp = TAT
+        # if phase == "S":  # flip trace
+        #     y = np.flip(y)
+        #     # y = -y  # polarity
+        #     TATp = (N-1)*dt - TAT
+        # else:
+        TATp = TAT
         # shorten vector
-        y = y[:][:round((TATp+50)/dt)]
+        # y = y[:][:round((TATp+50)/dt)]
         # create time vector
-        t = np.linspace(0-TATp, 50, len(y[0]))
+        t = np.linspace(-TATp, config.ta, len(y[0]))
         plt.close('all')
         fig = plt.figure()
-        fig, ax = plt.subplots(3, 1, sharex=True)
-        ax[2].set_xlabel('time in s')
-        ax[1].set_ylabel('normalised Amplitude')
+        fig, ax = plt.subplots(1, 3, sharex=True, sharey=True,
+                               gridspec_kw={'wspace': 0})
+        # ax[0].set_ylabel('time in s')
+        # ax[1].set_ylabel('normalised Amplitude')
         for ii, x in enumerate(ax):
-            x.plot(t, y[ii][:], color="k")
-            x.axvline(color='r')
-            x.set_xlim(-20, 50)
+            x.plot(y[ii][:], t, color="k", linewidth=1.5)
+            # x.axvline(color='r')
             x.set_title(ch[ii])
-            x.yaxis.set_major_formatter(ScalarFormatter())
+            x.xaxis.set_major_formatter(plt.NullFormatter())
             x.yaxis.major.formatter._useMathText = True
-            x.xaxis.set_major_formatter(ScalarFormatter())
+            x.yaxis.set_major_formatter(ScalarFormatter())
             x.xaxis.major.formatter._useMathText = True
             x.yaxis.set_minor_locator(AutoMinorLocator(5))
             x.xaxis.set_minor_locator(AutoMinorLocator(5))
-            # x.yaxis.set_label_coords(0.63, 1.01)
-            x.yaxis.tick_right()
-            x.grid(color='c', which='both', linewidth=.25)
-        # ax.legend(frameon=False, loc='upper left', ncol=2, handlelength=4)
-        # plt.legend(['it_max=4000', 'it_max=400'])
+            x.set_ylim(-100, 50)
+            # x.yticks([])
+            x.tick_params(axis ='x', which ='both', length = 0)
+            x.label_outer()
+        #     # x.yaxis.set_label_coords(0.63, 1.01)
+        #     # x.grid(color='c', which='both', linewidth=.25)
+        #             # ax.legend(frameon=False, loc='upper left', ncol=2, handlelength=4)
+        # # plt.legend(['it_max=4000', 'it_max=400'])
         plt.savefig(os.path.join(outloc + file[:-5] + "pdf"), dpi=dpi)