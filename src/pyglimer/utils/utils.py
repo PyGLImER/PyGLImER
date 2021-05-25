461b09ac2cf2b78ec5bb794cffd2ae17fbc98e2e
PeterMakus <peter.makus@gmx.de>
2021-05-25 09:03:58 +0200

    function to add channel to inventory

--- src/pyglimer/utils/utils.py
+++ src/pyglimer/utils/utils.py
@@ -13,6 +13,12 @@ Last Update: November 2019
 
 """
 
+import os
+
+from obspy import read_inventory, Inventory
+from obspy.core.inventory.station import Station
+from obspy.core.inventory.network import Network
+
 
 def dt_string(dt):
     """Returns Time elapsed string depending on how much time has passed.
@@ -37,3 +43,16 @@ def chunks(lst, n):
     """Yield successive n-sized chunks from lst. Useful for multi-threading"""
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
+
+def add_statxml_to_existing(stat: Station, netcode: str, dir: str):
+    net = Network(netcode, stations=[stat])
+    inv = Inventory(networks=[net])
+    station = inv[0][0].code
+    outfile = os.path.join(dir, '%s.%s.xml' % (netcode, station))
+    try:
+        old_inv = read_inventory(outfile)
+    except FileNotFoundError:
+        old_inv = Inventory()
+    old_inv.extend(inv)
+    # Write again to the same file
+    old_inv.write(outfile, 'STATIONXML')
