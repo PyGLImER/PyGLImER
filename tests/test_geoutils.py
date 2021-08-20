368c51150ed51387fd33aa0c18d8dba91f84e68e
PeterMakus <peter.makus@gmx.de>
2021-08-20 11:45:10 +0200

    threw out the ratings

---
+++ tests/test_geoutils.py
@@ -0,0 +1,15 @@
+'''
+:copyright:
+   The PyGLImER development team (makus@gfz-potsdam.de).
+:license:
+   `GNU Lesser General Public License, Version 3 <https://www.gnu.org/copyleft/lesser.html>`
+:author:
+   Peter Makus (makus@gfz-potsdam.de)
+
+Created: Friday, 20th August 2021 11:29:54 am
+Last Modified: Friday, 20th August 2021 11:30:12 am
+'''
+
+import unittest
+
+from pyglimer.utils import geo_utils as gu
