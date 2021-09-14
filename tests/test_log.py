f4ba99284783738ac62974ed6e5de667a34c99a1
PeterMakus <peter.makus@gmx.de>
2021-09-14 17:57:47 +0200

    logging test with pytest Should work now as travis now uses "pytest tests -p no:logging"

--- tests/test_log.py
+++ tests/test_log.py
@@ -7,7 +7,7 @@
    Peter Makus (makus@gfz-potsdam.de)
 
 Created: Tuesday, 14th September 2021 08:39:45 am
-Last Modified: Tuesday, 14th September 2021 10:36:22 am
+Last Modified: Tuesday, 14th September 2021 05:27:22 pm
 '''
 
 import logging
@@ -20,6 +20,8 @@ import numpy as np
 from pyglimer.utils import log
 
 
+# Those tests don't work on pytest
+# There, loglvl is always 0 and handlers are not assignerd?
 m = mock_open()
 
 
@@ -34,30 +36,35 @@ class TestStartLoggerIfNecessary(unittest.TestCase):
         for h in logger.handlers:
             if hasattr(h, 'baseFilename'):
                 self.assertEqual(os.path.basename(h.baseFilename), 'myfile')
+        # cleanup
+        while logger.hasHandlers():
+            logger.removeHandler(logger.handlers[0])
 
     @patch('builtins.open', m)
     def test_already_existing_handler(self):
         loglvl = np.random.randint(0, 4)*10
-        log.start_logger_if_necessary('mylogger2', 'myfile', loglvl)
+        log.start_logger_if_necessary('mylogger', 'myfile', loglvl)
         logger = log.start_logger_if_necessary(
-            'mylogger2', 'notmyfile', loglvl-10)
-        self.assertEqual(logger.name, 'mylogger2')
+            'mylogger', 'notmyfile', loglvl-10)
+        self.assertEqual(logger.name, 'mylogger')
         self.assertEqual(logger.level, loglvl)
         self.assertEqual(len(logger.handlers), 2)
         for h in logger.handlers:
             if hasattr(h, 'baseFilename'):
                 self.assertEqual(os.path.basename(h.baseFilename), 'myfile')
+        while logger.hasHandlers():
+            logger.removeHandler(logger.handlers[0])
 
 
 class TestCreateMPILogger(unittest.TestCase):
     @patch('builtins.open', m)
     def test_names(self):
         loglvl = np.random.randint(0, 4)*10
-        logger = log.start_logger_if_necessary('mylogger3', 'myfile', loglvl)
+        logger = log.start_logger_if_necessary('mylogger', 'myfile', loglvl)
         rank = np.random.randint(0, 999)
         rankstr = str(rank).zfill(3)
         mpilog = log.create_mpi_logger(logger, rank)
-        exp_name = 'mylogger3rank%s' % rankstr
+        exp_name = 'myloggerrank%s' % rankstr
         exp_fname = 'myfilerank%s' % rankstr
         self.assertEqual(mpilog.name, exp_name)
         self.assertEqual(mpilog.level, loglvl)
@@ -65,6 +72,10 @@ class TestCreateMPILogger(unittest.TestCase):
         for h in mpilog.handlers:
             if hasattr(h, 'baseFilename'):
                 self.assertEqual(os.path.basename(h.baseFilename), exp_fname)
+        while logger.hasHandlers():
+            logger.removeHandler(logger.handlers[0])
+        while mpilog.hasHandlers():
+            mpilog.removeHandler(mpilog.handlers[0])
 
     def test_no_fh(self):
         logger = logging.getLogger('testlogger')
