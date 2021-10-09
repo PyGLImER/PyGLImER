862e68d1b8b2effc080e478f8da8c24e60e93669
Lucas Sawade <lsawade@princeton.edu>
2021-10-09 15:07:59 -0400

    Removed dependency in utils that does not exist anymore

--- docs/conf.py
+++ docs/conf.py
@@ -64,7 +64,7 @@ html_theme = 'sphinx_book_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-
+html_title = "PyGLImER"
 html_logo = "chapters/figures/logo.png"
 # html_favicon = "chapters/figures/favicon.ico"
 
