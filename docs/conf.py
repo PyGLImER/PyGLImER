d72440a98f420fd26dd5f7ece53d05ea832c16d1
PeterMakus <peter.makus@gmx.de>
2022-10-19 11:35:48 -0700

    updated plots. E.g., now including pyglimer colors

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, the PyGLImER development team'
 author = 'Peter Makus, Lucas Sawade'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.3'
+release = '0.3.4'
 
 
 # -- General configuration ---------------------------------------------------
