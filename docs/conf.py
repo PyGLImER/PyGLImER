6f61fa709a797d6fd634b045a4e836d13aa81e81
PeterMakus <peter.makus@gmx.de>
2022-10-21 16:01:43 -0700

    fixed some pretty large bugs in info file writing

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, the PyGLImER development team'
 author = 'Peter Makus, Lucas Sawade'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.4'
+release = '0.3.5'
 
 
 # -- General configuration ---------------------------------------------------
