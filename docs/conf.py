a4871a21880dbe2a7ca0e9c8f70e608a9a285b99
Peter Makus <makus@gfz-potsdam.de>
2025-07-04 16:02:45 +0200

    stuff

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2025, the PyGLImER development team'
 author = 'Peter Makus, Lucas Sawade'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.5'
+release = '0.4.6'
 
 
 
